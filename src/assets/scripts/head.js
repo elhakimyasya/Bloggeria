82ca8d6689971b8a800877f914f5a01283df9429
Yasya El Hakim <yasyaelhakim@gmail.com>
2022-01-23 17:30:49 +0700

    Updated

--- src/assets/scripts/head.js
+++ src/assets/scripts/head.js
@@ -1,6 +1,7 @@
 var elcreativeConfig = {
     buttons: {
-        buttonSearchToggle: "button_toggle_search"
+        buttonSearchToggle: "button_toggle_search",
+        buttonSearchClose: "button_close_search",
     },
     elements: {
         elementSearchInput: "search_input"
--- src/assets/scripts/scripts.js
+++ src/assets/scripts/scripts.js
@@ -2,6 +2,14 @@
 if (document.getElementById(elcreativeConfig.buttons.buttonSearchToggle)) {
     document.getElementById(elcreativeConfig.buttons.buttonSearchToggle).addEventListener("click", () => {
         document.body.classList.toggle("--search_active");
-        elcreativeConfig.elements.elementSearchInput.focus();
+        document.getElementById(elcreativeConfig.elements.elementSearchInput).focus();
+    })
+};
+
+// Search Close
+if (document.getElementById(elcreativeConfig.buttons.buttonSearchClose)) {
+    document.getElementById(elcreativeConfig.buttons.buttonSearchClose).addEventListener("click", () => {
+        document.body.classList.remove("--search_active");
+        document.getElementById(elcreativeConfig.elements.elementSearchInput).blur();
     })
 };
