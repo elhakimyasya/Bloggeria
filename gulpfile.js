437c6d36631afa48d64955f59502f61b5f08d783
Yasya El Hakim <yasyaelhakim@gmail.com>
2022-08-09 21:11:54 +0700

    update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,17 @@
-const gulp = require('gulp');
+const fs = require("fs");
+const del = require("del");
+const gulp = require("gulp");
+const gulpSass = require('gulp-sass')(require('sass'));
 const gulpAutoPrefixer = require('gulp-autoprefixer');
 const gulpCleanCSS = require('gulp-clean-css');
+const gulpBabel = require('gulp-babel');
+const gulpBabelMinify = require('gulp-babel-minify');
 const gulpTokenReplace = require('gulp-token-replace');
+const gulpStripComments = require('gulp-strip-comments');
 const gulpFileInclude = require('gulp-file-include');
 const gulpReplace = require('gulp-replace');
 const gulpRename = require('gulp-rename');
-const gulpStripComments = require('gulp-strip-comments');
-const gulpBabel = require('gulp-babel');
-const gulpBabelMinify = require('gulp-babel-minify');
-const gulpSass = require('gulp-sass')(require('sass'));
-const del = require('del');
-const fs = require('fs');
+const gulpJsonMinify = require('gulp-jsonminify')
 
 const packages = './package.json';
 
@@ -29,7 +30,7 @@ gulp.task('styles', function () {
 });
 // Generate Prefixed Styles
 gulp.task('styles:prefixed', function () {
-    return gulp.src('./build/styles/*.{css,scss}')
+    return gulp.src('./build/styles/*.css')
         .pipe(gulpAutoPrefixer({
             cascade: false
         }))
@@ -37,7 +38,7 @@ gulp.task('styles:prefixed', function () {
 });
 // Generate Minified Styles
 gulp.task('styles:minify', function () {
-    return gulp.src('./src/assets/styles/*.{css,scss}')
+    return gulp.src('./build/styles/*.{css,scss}')
         .pipe(gulpCleanCSS({
             level: {
                 1: {
@@ -59,7 +60,7 @@ gulp.task('script', function () {
 });
 // Generate Minified Script
 gulp.task('script:minify', function () {
-    return gulp.src('build/scripts/*js')
+    return gulp.src('build/scripts/*.js')
         .pipe(gulpBabelMinify({
             mangle: {
                 keepClassName: false
@@ -70,6 +71,31 @@ gulp.task('script:minify', function () {
             removeConsole: true
         }))
         .pipe(gulp.dest('./build/scripts'))
+});
+
+// Generate JSON (Schema)
+gulp.task('json:minify', function () {
+    return gulp.src('./src/assets/scripts/json/*.json')
+        .pipe(gulpJsonMinify())
+        .pipe(gulp.dest('./build/scripts/json'))
+});
+// Generate Replaced JSON Data
+gulp.task('json:replace', function () {
+    return gulp.src('./build/scripts/json/*.json')
+        .pipe(gulpReplace('dataBlogTitleJsonEscaped', '<data:blog.title.jsonEscaped/>'))
+        .pipe(gulpReplace('dataBlogHomepageUrlJsonEscaped', '<data:blog.homepageUrl.jsonEscaped/>'))
+        .pipe(gulpReplace('dataBlogSearchUrl', '<data:blog.searchUrl.jsonEscaped/>'))
+        .pipe(gulpReplace('dataPostUrlCanonicalJsonEscaped', '<data:post.url.canonical.jsonEscaped/>'))
+        .pipe(gulpReplace('dataPostTitleJsonEscaped', '<data:post.title.jsonEscaped/>'))
+        .pipe(gulpReplace('dataPostBodySnippet', "<b:eval expr='(data:post.body snippet {length: 150, links: false, linebreaks: false, ellipsis: true}).jsonEscaped'/>"))
+        .pipe(gulpReplace('dataPostDateIso8601JsonEscaped', '<data:post.date.iso8601.jsonEscaped/>'))
+        .pipe(gulpReplace('dataPostLastUpdatedIso8601JsonEscaped', '<data:post.lastUpdated.iso8601.jsonEscaped/>'))
+        .pipe(gulpReplace('dataPostAuthorNameJsonEscaped', '<data:post.author.name.jsonEscaped/>'))
+        .pipe(gulpReplace('dataPostFeaturedImage', "<b:eval expr='data:post.featuredImage.isResizable ? resizeImage(data:post.featuredImage, 1200, &quot;1200:630&quot;) : &quot;https://lh3.googleusercontent.com/ULB6iBuCeTVvSjjjU1A-O8e9ZpVba6uvyhtiWRti_rBAs9yMYOFBujxriJRZ-A=w1200&quot;'/>"))
+        .pipe(gulpReplace('dataMessagesHome', '<data:messages.home/>'))
+        .pipe(gulpReplace('dataPostLabelsFirstName', '<b:eval expr="data:post.labels ? data:post.labels.first.name : data:messages.home" />'))
+        .pipe(gulpReplace('dataPostLabelsFirstUrlCanonical', '<b:eval expr="data:post.labels ? data:post.labels.first.url.canonical : data:blog.homepageUrl.canonical" />'))
+        .pipe(gulp.dest('./build/scripts/json'))
 });
 
 // Generate Timestamp for /package.json
@@ -116,9 +142,6 @@ gulp.task('start', function () {
         .pipe(gulpTokenReplace({
             global: tokenData
         }))
-        .pipe(gulpStripComments({
-            trim: true
-        }))
         .pipe(gulpFileInclude({
             indent: true,
             basepath: '@@file',
@@ -131,7 +154,16 @@ gulp.task('start', function () {
         }))
 
         .pipe(gulp.dest('./dist'))
-})
+});
+
+// Strip Comments
+gulp.task('comments', function () {
+    return gulp.src('dist/theme.xml')
+        .pipe(gulpStripComments({
+            trim: true
+        }))
+        .pipe(gulp.dest('./dist'))
+});
 
 // Production Mode
 gulp.task('build:production', gulp.series(
@@ -141,8 +173,11 @@ gulp.task('build:production', gulp.series(
     'styles:minify',
     'script',
     'script:minify',
+    'json:minify',
+    'json:replace',
     'timestamp',
     'start',
+    'comments'
 ));
 // Development Mode
 gulp.task('build:development', gulp.series(
@@ -150,4 +185,4 @@ gulp.task('build:development', gulp.series(
     'styles',
     'script',
     'start',
-));
+))
