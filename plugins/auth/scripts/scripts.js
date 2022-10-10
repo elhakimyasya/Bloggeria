2932152b4f3b04b5ee9959050877e7c669e0c1fa
Yasya El Hakim <yasyaelhakim@gmail.com>
2022-10-10 19:30:16 +0700

    update 6 files

--- plugins/auth/scripts/scripts.js
+++ plugins/auth/scripts/scripts.js
@@ -1,7 +1,25 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
 ((dashboardPage, options) => {
-    if (location.href.indexOf(dashboardPage) != -1) {
-        initia
-    }
+    // Check Dashboard Page
+    if (location.href.indexOf(dashboardPage) == -1) return;
+
+    initializeApp(options.firebaseConfig);
+
+    const firestoreDatabase = getFirestore();
+    const firebaseAuth = getAuth();
+
+    onAuthStateChanged(firebaseAuth, (currentUser) => {
+        if (currentUser) {
+            console.log('user');
+            functionSnackbar('pleasewaitpleasewaitpleasewaitpleasewaitpleasewaitpleasewaitpleasewaitpleasewaitpleasewaitpleasewaitpleasewaitpleasewaitpleasewaitpleasewaitpleasewaitpleasewait', 5000)
+        } else {
+            console.log('notuser')
+            functionSnackbar('pleasewaitpleasewaitpleasewaitpleasewaitpleasewaitpleasewaitpleasewaitpleasewaitpleasewaitpleasewaitpleasewaitpleasewaitpleasewaitpleasewaitpleasewaitpleasewait', 5000)
+        }
+    });
 })(authPageIndex, {
     firebaseConfig: firebaseConfig,
 })
--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,15 @@ const path = require('path');
 module.exports = {
     mode: 'production',
     entry: {
-        scripts: './src/assets/scripts/scripts.js',
+        // scripts: './src/assets/scripts/scripts.js',
+        auth: './plugins/auth/scripts/scripts.js',
+    },
+    optimization: {
+        chunkIds: 'named',
     },
     output: {
         path: path.resolve(__dirname, './build/scripts'),
         filename: '[name].js'
     },
-    watch: false
+    watch: true
 }
