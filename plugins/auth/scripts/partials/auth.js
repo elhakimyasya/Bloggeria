30b938e96c934f7dcdf83a81d7f5228d1e693f6f
Yasya El Hakim <yasyaelhakim@gmail.com>
2022-10-14 01:14:10 +0700

    delete 7 files

--- plugins/auth/scripts/partials/auth.js
+++
@@ -1,9 +0,0 @@
-import { signInWithRedirect } from "firebase/auth";
-
-const auth = (authModule, authProvider) => {
-    signInWithRedirect(authModule, authProvider).then(() => {
-        functionSnackbar('pleasewaitbro', 5000)
-    })
-}
-
-export default auth;
