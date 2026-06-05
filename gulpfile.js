f6bf09bf772b555ef7cfa4e54a91ece67ec8066f
Yasya El Hakim <yasyaelhakim@gmail.com>
2026-06-05 17:15:34 +0700

    chore: update dependencies and improve HTML structure
    - Updated @babel/preset-env from 7.29.5 to 7.29.7
    - Added @tailwindcss/cli as a new dependency (version 4.3.0)
    - Updated npm-check-updates from 22.2.0 to 22.2.2
    - Updated sass from 1.99.0 to 1.100.0
    - Updated terser-webpack-plugin from 5.6.0 to 5.6.1
    - Updated webpack from 5.106.2 to 5.107.2
    - Modified releasedDate in package.json
    - Improved formatting in main.html for better readability

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,6 @@ const gulpRename = require('gulp-rename');
 const gulpStripComments = require('gulp-strip-comments');
 const fs = require('fs');
 const gulpBabel = require('gulp-babel');
-const gulpHTMLMin = require('gulp-htmlmin');
 
 const generateTimestamp = () => {
     const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
@@ -58,7 +57,6 @@ const updatePackageTimestamp = () => {
     });
 };
 
-// Clean everything inside ./build directory
 gulp.task('clean', () => {
     const sources = ['./build/**'];
 
@@ -67,46 +65,39 @@ gulp.task('clean', () => {
     });
 });
 
-// Generate styles
 gulp.task('styles', () => {
-    const sources = ['./src/assets/styles/*.{css,scss}', '!./src/assets/styles/tailwind.css'];
+    const sources = [
+        './src/assets/styles/*.{css,scss}',
+        '!./src/assets/styles/tailwind.css'
+    ];
 
     return gulp.src(sources).pipe(gulpSass()).pipe(gulp.dest('./build/styles'));
 });
 
-// Generate prefixed styles
 gulp.task('styles:autoprefixed', () => {
     const sources = ['./build/styles/*.{css}'];
 
-    return gulp
-        .src(sources)
-        .pipe(
-            gulpAutoPrefixer({
-                cascade: false
-            })
-        )
-        .pipe(gulp.dest('./build/styles'));
+    return gulp.src(sources).pipe(gulpAutoPrefixer({
+        cascade: false
+    })).pipe(gulp.dest('./build/styles'));
 });
 
-// Generate minified styles
 gulp.task('styles:minified', () => {
     const sources = ['./build/styles/*.{css,scss}'];
 
-    return gulp
-        .src(sources)
-        .pipe(
-            gulpCleanCSS({
-                level: {
-                    1: {
-                        specialComments: 0
-                    }
-                }
-            })
-        )
-        .pipe(gulp.dest('./build/styles'));
+    return gulp.src(sources).pipe(gulpCleanCSS({
+        level: {
+            1: {
+                specialComments: 0
+            }
+        }
+    })).pipe(gulp.dest('./build/styles'));
 });
 
-// Generate scripts (webpack)
+gulp.task('styles:remove-property', () => {
+    return gulp.src('./build/styles/*.css').pipe(gulpReplace(/@property\s+--[\w-]+\{[^{}]*\}/g, '')).pipe(gulp.dest('./build/styles'));
+});
+
 gulp.task('scripts:webpack', () => {
     const sources = {
         scripts: './src/assets/scripts/scripts.js'
@@ -172,123 +163,94 @@ gulp.task('scripts:webpack', () => {
     return gulp.src(sources.scripts).pipe(webpackStream(webpackConfig, webpack)).pipe(gulp.dest('./build/scripts'));
 });
 
-// Generate scripts (babel)
 gulp.task('scripts:babel', () => {
     const sources = ['./build/scripts/*.js'];
 
     return gulp.src(sources).pipe(gulpBabel()).pipe(gulp.dest('./build/scripts'));
 });
 
-// Generate minified scripts
 gulp.task('scripts:minified', () => {
-    const sources = ['./build/scripts/*.js', './src/assets/scripts/scripts-head.js'];
+    const sources = [
+        './build/scripts/*.js',
+        './src/assets/scripts/scripts-head.js'
+    ];
 
-    return gulp
-        .src(sources)
-        .pipe(
-            gulpBabelMinify({
-                mangle: {
-                    keepClassName: true
-                },
-                evaluate: false,
-                builtIns: false,
-                removeDebugger: true,
-                removeConsole: false
-            })
-        )
-        .pipe(gulp.dest('./build/scripts'));
+    return gulp.src(sources).pipe(gulpBabelMinify({
+        mangle: {
+            keepClassName: true
+        },
+        evaluate: false,
+        builtIns: false,
+        removeDebugger: true,
+        removeConsole: false
+    })).pipe(gulp.dest('./build/scripts'));
 });
 
-// Generate JSON (Schema)
 gulp.task('json:minify', () => {
     const sources = ['./src/assets/scripts/json/*.json'];
 
     return gulp.src(sources).pipe(gulpJsonMinify()).pipe(gulp.dest('./build/scripts/json'));
 });
 
-// Generate Replaced JSON Data
 gulp.task('json:replace', () => {
     const sources = ['./build/scripts/json/*.json'];
 
     return gulp.src(sources).pipe(gulpReplace('dataBlogTitle', '<data:blog.title.jsonEscaped/>')).pipe(gulpReplace('dataBlogHomepageUrl', '<data:blog.homepageUrl.jsonEscaped/>')).pipe(gulpReplace('dataBlogSearchUrl', '<data:blog.searchUrl.jsonEscaped/>')).pipe(gulpReplace('dataBlogLocale', '<data:blog.locale/>')).pipe(gulpReplace('dataBlogMetaDescription', "<b:eval expr='data:blog.metaDescription ? data:blog.metaDescription.escaped : data:view.description.escaped'/>")).pipe(gulpReplace('dataPostUrlCanonical', '<data:post.url.canonical.jsonEscaped/>')).pipe(gulpReplace('dataPostTitle', '<data:post.title.jsonEscaped/>')).pipe(gulpReplace('dataPostBodySnippet', "<b:eval expr='(data:post.body snippet {length: 150, links: false, linebreaks: false, ellipsis: true}).jsonEscaped'/>")).pipe(gulpReplace('dataPostDateIso8601', '<data:post.date.iso8601.jsonEscaped/>')).pipe(gulpReplace('dataPostLastUpdatedIso8601', '<data:post.lastUpdated.iso8601.jsonEscaped/>')).pipe(gulpReplace('dataPostAuthorName', '<data:post.author.name.jsonEscaped/>')).pipe(gulpReplace('dataPostFeaturedImage', "<b:eval expr='data:post.featuredImage.isResizable ? resizeImage(data:post.featuredImage, 1200, &quot;1200:630&quot;) : &quot;https://lh3.googleusercontent.com/ULB6iBuCeTVvSjjjU1A-O8e9ZpVba6uvyhtiWRti_rBAs9yMYOFBujxriJRZ-A=w1200&quot;'/>")).pipe(gulpReplace('dataMessagesHome', '<data:messages.home/>')).pipe(gulpReplace('dataPostLabelsFirstName', '<b:eval expr="data:post.labels ? data:post.labels.first.name : data:messages.home" />')).pipe(gulpReplace('dataPostLabelsFirstUrlCanonical', '<b:eval expr="data:post.labels ? data:post.labels.first.url.canonical : data:blog.homepageUrl.canonical" />')).pipe(gulp.dest('./build/scripts/json'));
 });
 
-// Remove all comments
 gulp.task('comments', () => {
     const sources = ['./dist/*.{xml,html}'];
 
-    return gulp
-        .src(sources)
-        .pipe(
-            gulpStripComments({
-                trim: true
-            })
-        )
-        .pipe(gulp.dest('./dist'));
+    return gulp.src(sources).pipe(gulpStripComments({
+        trim: true
+    })).pipe(gulp.dest('./dist'));
 });
 
-// Generate Timestamp
 gulp.task('timestamp', (results) => {
     updatePackageTimestamp();
     results();
 });
 
-// Minify HTML
 gulp.task('html:minify', (done) => {
     done();
 });
 
-// Final Tasks
 gulp.task('start', () => {
     const sources = ['./src/main.html'];
 
-    return gulp
-        .src(sources)
-        .pipe(
-            gulpTokenReplace({
-                global: packages
-            })
-        )
-        .pipe(
-            gulpFileInclude({
-                indent: true,
-                basepath: '@@file',
-                prefix: '@@'
-            })
-        )
-        .pipe(gulpReplace('-tw', '-elcreative'))
-        .pipe(
-            gulpRename({
-                basename: `theme-v${packages.version}`,
-                // basename: `${packages.names.replace(/\s/g, '-')}-v${packages.version}`,
-                extname: '.xml'
-            })
-        )
-        .pipe(gulp.dest('./dist'));
+    return gulp.src(sources).pipe(gulpTokenReplace({
+        global: packages
+    })).pipe(gulpFileInclude({
+        indent: true,
+        basepath: '@@file',
+        prefix: '@@'
+    })).pipe(gulpReplace('-tw', '-elcreative')).pipe(gulpRename({
+        basename: `theme-v${packages.version}`,
+        // basename: `${packages.names.replace(/\s/g, '-')}-v${packages.version}`,
+        extname: '.xml'
+    })).pipe(gulp.dest('./dist'));
 });
 
 // Build task: Production Mode
-gulp.task(
-    'build:production',
-    gulp.series(
-        'clean',
+gulp.task('build:production', gulp.series(
+    'clean',
 
-        'styles',
-        'styles:autoprefixed',
-        'styles:minified',
+    'styles',
+    'styles:autoprefixed',
+    'styles:minified',
+    'styles:remove-property',
 
-        'scripts:webpack',
-        // 'scripts:babel',
-        'scripts:minified',
+    'scripts:webpack',
+    // 'scripts:babel',
+    'scripts:minified',
 
-        'json:minify',
-        'json:replace',
+    'json:minify',
+    'json:replace',
 
-        'html:minify',
+    'html:minify',
 
-        'timestamp',
+    'timestamp',
 
-        'start',
-        'comments'
-    )
-);
+    'start',
+    'comments'
+));
